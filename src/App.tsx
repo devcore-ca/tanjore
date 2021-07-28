c90743823e7c1306e9694b0ee5f111ecb42db371
janac <>
2021-07-27 22:21:33 -0400

    Rename

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import { HomePage } from './components/home/HomePage.component';
+import Menu from './components/menu/Menu.component'
+
+function App() {
+  return (
+    <Router>
+      <Link to="/home">Home</Link>
+      <Link to="/menu">Menu</Link>
+
+      <Switch>
+
+        <Route path="/menu">
+          <Menu />
+        </Route>
+
+        <Route path="/">
+          <HomePage />
+        </Route>
+
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
