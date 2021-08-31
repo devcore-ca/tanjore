6ca2b2d1f843922716b835df2f75ec6d7c072717
Vikram Thangavel <76668807+VikramT19@users.noreply.github.com>
2021-08-31 17:59:09 -0400

    Update Home.component.tsx

--- src/components/home/Home.component.tsx
+++ src/components/home/Home.component.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import styles from './Home.module.css';
+import Footer from './components/footer/Footer';
 
 function Home() {
   return (
     <div className={styles.homePage}>Home Page of Tanjore</div>
+    <Footer/>
   );
 }
 
