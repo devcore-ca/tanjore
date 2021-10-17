4f1f5a15dc17cf1403b61ef24a82c8b41548c8d9
Kavinath <kkane066@uottawa.ca>
2021-10-16 23:30:54 -0400

    Responsive UI for About Us

--- src/components/about/About.component.tsx
+++ src/components/about/About.component.tsx
@@ -4,7 +4,16 @@ import about from "./images/rak.png";
 import about1 from "./images/rak2.png";
 import about2 from "./images/rak3.png";
 import about3 from "./images/rak4.png";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
+import { isTemplateHead } from "typescript";
 import aboutus from "./images/aboutus.png";
+
+const Item = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(1),
+  textAlign: "left",
+}));
 
 function About() {
   return (
@@ -14,60 +23,90 @@ function About() {
         src={aboutus}
         style={{ width: "100%", position: "relative" }}
       />
+
       <div>
-        <div className={styles.flex}>
-          <img className={styles.imageSize} src={about} alt=""></img>
-          <div>
-            <h1 className={styles.h1}> Our Story </h1>
-            <p className={styles.p}>
-              Rakesh Kumar came to Canada from MP, India in 1989 with a passion
-              for food and culinary arts. He was offered a head chef position at a
-              popular local Indian restaurant in Scarborough where he worked for
-              over 10 years.
-            </p>
-          </div>
-        </div>
+        <Grid
+          container
+          spacing={2}
+          rowSpacing={4}
+          columns={12}
+          className={styles.flex}
+        >
+          <Grid item xs={12} sm={12} md={6} lg={6}>
+            <Item>
+              <img className={styles.imageSize} src={about} alt=""></img>
+            </Item>
+          </Grid>
+          <Grid item xs={12} sm={12} md={6} lg={6}>
+            <Item>
+              <h1 className={styles.h1}> Our Story </h1>
+              <p className={styles.p}>
+                Rakesh Kumar came to Canada from MP, India in 1989 with a
+                passion for food and culinary arts. He was offered a head chef
+                position at a popular local Indian restaurant in Scarborough
+                where he worked for over 10 years.
+              </p>
+            </Item>
+          </Grid>
 
-        <div>
-          <div className={styles.flex}>
-            <p className={styles.p2}>
-              In 2004, Rakesh finally decided he wanted to put his culinary skills
-              to the test and he and his wife Neeru opened their very own little
-              takeout restaurant called Tanjore Catering & Sweets. This quickly
-              became a local favourite!
-            </p>
-            <div>
-              <img className={styles.imageSize2} src={about1} alt=""></img>
-            </div>
-          </div>
-        </div>
+          <Grid container columns={12} className={styles.flex}>
+            <Grid item xs={12} sm={12} md={6} lg={6}>
+              <Item>
+                <p className={styles.p2}>
+                  In 2004, Rakesh finally decided he wanted to put his culinary
+                  skills to the test and he and his wife Neeru opened their very
+                  own little takeout restaurant called Tanjore Catering &amp;
+                  Sweets. This quickly became a local favourite!
+                </p>
+              </Item>
+            </Grid>
 
-        <div className={styles.flex}>
-          <img className={styles.imageSize3} src={about2} alt=""></img>
-          <div>
-            <p className={styles.p3}>
-              Wanting to provide an authentic dine in experience, in 2017 they
-              decided to take the leap and expand their restaurant to a larger
-              location, providing a buffet style lunch 6 days a week. Customers
-              were coming from different cities to try out our savoury dishes.
-              Some of our popular must tries are the Palace Naan, the Biriyanis
-              and of course our mouth-watering butter chicken!
-            </p>
-          </div>
-        </div>
+            <Grid item xs={12} sm={12} md={6} lg={6}>
+              <Item>
+                <img className={styles.imageSize2} src={about1} alt=""></img>
+              </Item>
+            </Grid>
+          </Grid>
+        </Grid>
 
-        <div>
-          <div className={styles.flex}>
-            <p className={styles.p4}>
-              Today, Tanjore is an admired family restaurant in Belleville, ON and
-              even though we had to leave our dedicated customers in Scarborough,
-              it definitely hasn’t stopped them from travelling here!
-            </p>
-            <div>
+        <Grid>
+          <Grid item xs={12} sm={12} md={12} lg={12}>
+            <Item>
+              <img className={styles.imageSize3} src={about2} alt=""></img>
+            </Item>
+          </Grid>
+          <Grid item xs={12} sm={12} md={12} lg={12}>
+            <Item className={styles.p3}>
+              <p>
+                Wanting to provide an authentic dine in experience, in 2017 they
+                decided to take the leap and expand their restaurant to a larger
+                location, providing a buffet style lunch 6 days a week.
+                Customers were coming from different cities to try out our
+                savoury dishes. Some of our popular must tries are the Palace
+                Naan, the Biriyanis and of course our mouth-watering butter
+                chicken!
+              </p>
+            </Item>
+          </Grid>
+        </Grid>
+
+        <Grid>
+          <Grid item xs={12} sm={12} md={12} lg={12}>
+            <Item>
+              <p className={styles.p4}>
+                Today, Tanjore is an admired family restaurant in Belleville, ON
+                and even though we had to leave our dedicated customers in
+                Scarborough, it definitely hasn’t stopped them from travelling
+                here!
+              </p>
+            </Item>
+          </Grid>
+          <Grid item xs={12} sm={12} md={12} lg={12}>
+            <Item>
               <img className={styles.imageSize4} src={about3} alt=""></img>
-            </div>
-          </div>
-        </div>
+            </Item>
+          </Grid>
+        </Grid>
       </div>
     </>
   );
