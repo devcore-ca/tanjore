00a64c123b78a610ecebcd3b346bc5e52f9c670d
supavannan <supavannan@gmail.com>
2021-11-20 01:21:25 -0500

    Editted Gallery to use Swiper.js

--- src/components/gallery/SwiperGallery.component.tsx
+++ src/components/gallery/SwiperGallery.component.tsx
@@ -1,22 +1,87 @@
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
+// Core modules imports are same as usual
+import { Navigation, Pagination } from "swiper";
+// Direct React component imports
+import { Swiper, SwiperSlide } from "swiper/react/swiper-react.js";
+import React from "react";
 
-// Import Swiper styles
-import "swiper/css";
+// Styles must use direct files imports
+import "swiper/swiper.scss"; // core Swiper
+// import "swiper/swiper-bundle.min.css";
+import "swiper/modules/navigation/navigation.scss"; // Navigation module
+import "swiper/modules/pagination/pagination.scss"; // Pagination module
+import styles from "./SwiperGallery.module.css";
+
+import image1 from "../../common/images/IMG_1.png";
+import image2 from "../../common/images/IMG_2.png";
+import image3 from "../../common/images/IMG_3.png";
+import image4 from "../../common/images/IMG_4.png";
+import image5 from "../../common/images/IMG_5.png";
+import image6 from "../../common/images/IMG_6.png";
+import image7 from "../../common/images/IMG_7.png";
+import image8 from "../../common/images/IMG_8.png";
+
+const sourceImages = [
+  {
+    original: image1,
+    description: "Authentic Tanjore Combo with Garlic Naan",
+  },
+  {
+    original: image2,
+    description: "Chicken Biryani",
+  },
+  {
+    original: image3,
+    description: "Butter Paneer",
+  },
+  {
+    original: image4,
+    description: "Chilli Chicken",
+  },
+  {
+    original: image5,
+    description: "Veggie Briyani",
+  },
+  {
+    original: image6,
+    description: "Pani Poori Chaat",
+  },
+  {
+    original: image7,
+    description: "Chicken Pakora",
+  },
+  {
+    original: image8,
+    description: "Bhel Sev Chaat",
+  },
+];
+
+const Slides = sourceImages.map((image, index) => {
+  return (
+    <div className={styles.slideWrapper}>
+      <SwiperSlide>
+        <div className={styles.imageContainer}>
+          <span className={styles.description}>{image.description}</span>
+          <img className={styles.image} src={image.original} />
+        </div>
+      </SwiperSlide>
+    </div>
+  );
+});
 
 export default () => {
   return (
     <Swiper
+      modules={[Navigation, Pagination]}
       spaceBetween={50}
-      slidesPerView={3}
+      slidesPerView={1}
       onSlideChange={() => console.log("slide change")}
       onSwiper={(swiper) => console.log(swiper)}
+      pagination={{ clickable: true }}
+      navigation
+      scrollbar={{ draggable: true }}
+      className={styles.slideContainer}
     >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      ...
+      {Slides}
     </Swiper>
   );
 };
